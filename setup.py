6d3ff80a46cf05a4b90fb0ede0fd54acd4463509
Harshit Raj <iamrajharshit@gmail.com>
2024-02-18 16:43:54 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Gen Ai with OpenAI",
+    version="0.0.0",
+    author="rajharshit",
+    author_email="iamrajharshit@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
